567e83eb53c93edb0bbfc7cf837ccdab31568c92
sandisaputraa <104289275+sandisaputraa@users.noreply.github.com>
2022-05-21 08:08:40 +0700

    latihan1.py

---
+++ latihan1.py
@@ -0,0 +1,9 @@
+#latihan 1: Biodata Idola
+
+print('Biodata Idola')
+print('nama : Bill Gates')
+print('umur : 66 tahun')
+print('alamat : Medina, Wangshington, AS')
+print('perusahaan : Microsoft ')
+print('kelahiran : 28 Oktober 1955')
+print('kekayaan bersih : 125 miliar USD')
