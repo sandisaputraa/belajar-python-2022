b213a282a2b044a63966d36724e8c899d61bf173
sandisaputraa <104289275+sandisaputraa@users.noreply.github.com>
2022-05-21 08:13:30 +0700

    latihan7.py

---
+++ latihan7.py
@@ -0,0 +1,8 @@
+#latihan 7 Program menghitung luas segitiga
+# Rumus luas segitiga adalah : (a x t)/2
+
+print("Program Untuk Menghitung Luas Segitiga\n")
+panjang_alas = float(input("Masukkan nilai alas yang akan dihitung (dalam cm) :"))
+tinggi = float(input("Masukkan nilai tinggi yang akan dihitung (dalam cm)"))
+hasil = ((panjang_alas * tinggi)/2)
+print("Jadi Luas Area Segitiga adalah :",hasil, "centimeter persegi")
