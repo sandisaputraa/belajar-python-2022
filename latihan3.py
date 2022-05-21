ef0f638ec0554926884fa56c12f6d3bdd557e55f
sandisaputraa <104289275+sandisaputraa@users.noreply.github.com>
2022-05-21 08:10:35 +0700

    latiihan3.py

---
+++ latihan3.py
@@ -0,0 +1,8 @@
+#latihan 3: program menghitung luas ruangan
+#luas ruangan adalah panjang x lebar
+
+print('Menghitug Luas Ruangan')
+panjang=float(input('panjang : '))
+lebar=float(input('lebar : '))
+hasil = panjang*lebar
+print("jadi, luas ruangan adalah ",hasil,"Meter")
