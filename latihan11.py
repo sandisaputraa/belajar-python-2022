f97eb598881cedf8c4343561b478c99afe038211
sandisaputraa <104289275+sandisaputraa@users.noreply.github.com>
2022-05-21 11:38:25 +0700

    Create latihan11.py

---
+++ latihan11.py
@@ -0,0 +1,21 @@
+#latihan 11
+
+sisi=int(input('sisi = '))
+if(sisi==3):
+    print('itu adalah bentuk segitiga')
+elif(sisi==4):
+    print('itu adalah bentuk segiempat')
+elif(sisi==5):
+    print("itu adalah bentuk pentagon")
+elif(sisi==6):
+    print("itu adalah bentuk heksagon")
+elif(sisi==7):
+    print("itu adalah bentuk heptagon")
+elif(sisi==8):
+    print("itu adalah bentuk oktagon")
+elif(sisi==9):
+    print("itu adalah bentuk nonagon")
+elif(sisi==10):
+    print("itu adalah bentuk dekagon")
+else:
+    print("maaf, ilmu saya belum cukup")
